4fd741892f46ac24c7af194da32519992e5e5ad8
myros <myros.net@gmail.com>
2022-11-25 12:41:35 -0500

    vault login

--- vault.tf
+++ vault.tf
@@ -1,28 +1,36 @@
-resource "vault_namespace" "ns1" {
-  path = "ns1"
+resource "vault_namespace" "user" {
+  path = "user"
 }
 
 resource "vault_mount" "kvv2" {
+  namespace = vault_namespace.user.path
   path        = "kvv2"
   type        = "kv"
-  namespace = vault_namespace.ns1.path
   options     = { version = "2" }
   description = "KV Version 2 secret engine mount"
 }
 
 resource "vault_mount" "kvv3" {
+  namespace = vault_namespace.user.path
   path        = "kvv3"
   type        = "kv"
-  namespace = vault_namespace.ns1.path
   options     = { version = "2" }
   description = "KV Version 2 secret engine mount"
 }
 
+resource "vault_policy" "admin_namespace_policy" {
+  namespace = vault_namespace.user.path
+  name   = "administrators"
+  policy = file("policies/admin-ns-policy.hcl")
+}
+
 resource "vault_token" "token" {
   # role_name = "user"
+  namespace = vault_namespace.user.path
 
-  policies = ["policy1", "policy2"]
-  namespace = vault_namespace.ns1.path
+  policies = ["policy1", "policy2", vault_policy.admin_namespace_policy.name]
+  no_default_policy = true
+
   renewable = true
   ttl = "24h"
 
