64b691431b7b8d33c25e24ca0c2a53fc4880c53e
myros <myros.net@gmail.com>
2022-11-27 00:46:51 -0500

    Updates

--- providers.tf
+++ providers.tf
@@ -10,21 +10,4 @@ terraform {
       name = "vault-test"
     }
   }
-  
-  required_providers {
-    vault = {
-      version = "3.11.0"
-    }
-    restapi = {
-      source = "fmontezuma/restapi"
-    }
-  }
-}
-
-provider "vault" {
-  address = var.vault_addr // var.vault-url
-  skip_tls_verify = true
-  # skip_child_token = true
-
-  token = var.vault_token // var.token
 }
