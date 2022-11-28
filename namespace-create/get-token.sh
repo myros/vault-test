da4f2b2c6218c0b3f6b9d95778ed29aafc1923b3
myros <myros.net@gmail.com>
2022-11-27 20:19:25 -0500

    Updates

--- namespace-create/get-token.sh
+++ namespace-create/get-token.sh
@@ -17,7 +17,7 @@ eval "$(jq -r '@sh "ADDR=\(.addr) TOKEN=\(.token) NAMESPACE=\(.namespace)"')"
 
 # EXECUTE COMMAND 
 
-NEW_TOKEN=$(curl --location --request POST -H X-Vault-Namespace:${NAMESPACE} -H X-Vault-Token:${TOKEN} --data "$(generate_post_data)" ${ADDR}/v1/auth/token/create | jq -r .auth.client_token)
+NEW_TOKEN=$(curl --location --request POST -H X-Vault-Token:${TOKEN} --data "$(generate_post_data)" ${ADDR}/v1/auth/token/create | jq -r .auth.client_token)
 
 # WILL BE RETURNED AS JSON RESULT
 
