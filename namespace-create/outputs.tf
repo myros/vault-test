7fedc574cc32c1d11796dd69fac43621b1ed5555
myros <myros.net@gmail.com>
2022-11-28 13:05:32 -0500

    Updates

--- namespace-create/outputs.tf
+++ namespace-create/outputs.tf
@@ -4,6 +4,13 @@
 
 output "admin_token" {
   value = data.external.get_token.result["token"]
+
+
+  depends_on = [
+    external.get_token,
+    vault_namespace.user_namespace,
+    vault_policy.admin_namespace_policy
+  ]
 }
 
 output "namespace" {
